89044380296332297c9dcffc623fb7429ff34e9d
Luca Temperini <luca.temperini@mobgen.com>
2014-11-21 18:47:21 +0100

    changed to automatically create huge words

--- index.php
+++ index.php
@@ -1,28 +1,59 @@
 <?php
-//Get the Magic value that is calculated created a sha1 for each letter 
-//and get the ascii value added for each letters
-function getMagicValue($words){
-		for ($i = 0; $i < strlen($words); $i++){
-				$sha = str_split(sha1($words[$i]));
-				        $value=0;
-									foreach ($sha as $l)
-												$value += ord($l);
-										}
-			
-			return $value; 
+
+function createRandomWord(array $describeWord, $nTime) {
+	$str = null;
+	foreach ($describeWord as $value) {
+		for ($i = 1; $i <= $nTime; $i++) {
+			$str .= $value;
+		}
+	}
+
+	return str_shuffle($str);
 }
 
-$first  = 'abc';
-$second = 'aad';
+//Get the Magic value that is calculated created a sha1 for each letter
+//and get the ascii value added for each letters
+function getMagicValue($word, $method = null) {
+	$value = 0;
+	for ($i = 0; $i < strlen($word); $i++) {
+		switch ($method) {
+			case 'md5':
+				$ascii = str_split(md5($word[$i]));
+				foreach ($ascii as $l) {
+					$value += ord($l);
+				}
+
+				break;
+
+			case 'sha1':
+				$ascii = str_split(sha1($word[$i]));
+				foreach ($ascii as $l) {
+					$value += ord($l);
+				}
+				break;
+
+			default:
+				$value += ord($word[$i]);
+				break;
+		}
+	}
+
+	return $value;
+}
+
+
 //note that using 'abc' and 'aad' using only ord wouldn't work (with sha1 yes)
+$first  = createRandomWord(array('a', 'b', 'c', 'd', 'e',), 100000);
+$second = createRandomWord(array('a', 'b', 'c', 'd', 'e',), 100000);
 
-$start = microtime();
-$result = (strlen($first)=== strlen($second) 
-					&& getMagicValue($first) === getMagicValue($second)) 
-				? 'It is ' 
-							: 'It is NOT ';
-							$time = microtime() - $start;
+$start  = microtime(true);
+$result = (strlen($first) === strlen($second)
+	&& getMagicValue($first, 'md5') === getMagicValue($second, 'md5'))
+	? 'It is '
+	: 'It is NOT ';
 
-							echo "$result an anagram!! ({$time} microseconds)";
+$time = microtime(true) - $start;
 
-							?>
+echo "$result an anagram!! ({$time} microseconds)";
+
+?>
